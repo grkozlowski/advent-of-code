4c2df6e9c390aab9c03fa8c5686a58f2554d566f
grkozlowski <gr.kozlowski+github@gmail.com>
2023-12-09 11:19:31 +0100

    day 9

---
+++ src/main/java/com/example/aoc/Day09_2.java
@@ -0,0 +1,54 @@
+package com.example.aoc;
+
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Day09_2 {
+
+    private static final String testText = """
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+                """;
+    private static long result = 0L;
+
+    public static void main(String[] args) {
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("input/day09.txt"));
+//            BufferedReader reader = new BufferedReader(new StringReader(testText));
+
+            String line = reader.readLine();
+            while (line != null) {
+                Integer[] values = Arrays.stream(line.split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
+                result += calculateNextValue(values);
+                line = reader.readLine();
+            }
+
+            System.out.println("result:"+result);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static int calculateNextValue(Integer[] values) {
+        if (allZeros(values)) {
+            return 0;
+        }
+
+        Integer[] diffs = new Integer[values.length - 1];
+        for (int i = 1; i < values.length; ++i) {
+            diffs[i - 1] = values[i] - values[i - 1];
+        }
+
+        return values[0] - calculateNextValue(diffs);
+    }
+
+    private static boolean allZeros(Integer[] values) {
+        return Arrays.stream(values).allMatch(v -> v == 0);
+    }
+
+
+}

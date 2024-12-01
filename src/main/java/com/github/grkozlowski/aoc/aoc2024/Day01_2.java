b8a1a3b98cb13c9e2bcb37bc8ee072caed004b3a
grkozlowski <gr.kozlowski+github@gmail.com>
2024-12-01 06:44:57 +0100

    AOC 2024 | Day 01

--- src/main/java/com/github/grkozlowski/aoc/aoc2024/Day01_2.java
+++ src/main/java/com/github/grkozlowski/aoc/aoc2024/Day01_2.java
@@ -5,11 +5,13 @@ import com.github.grkozlowski.aoc.utils.InputType;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.github.grkozlowski.aoc.aoc2024.YearConstantValues.INPUT_FOLDER_PREFIX;
 
-public class Day01_1 {
+public class Day01_2 {
 
     private static final String DAY_NUMBER = "01";
     private static final InputType INPUT_TYPE = InputType.LARGE;
@@ -22,18 +24,17 @@ public class Day01_1 {
             BufferedReader reader = new BufferedReader(new FileReader(INPUT_FOLDER_PREFIX + INPUT_TYPE.subPath() + "day" + DAY_NUMBER + ".txt"));
             List<String> input = reader.lines().toList();
             List<Long> left = new ArrayList<>();
-            List<Long> right = new ArrayList<>();
+            Map<Long, Long> right = new HashMap<>();
 
             for (String line : input) {
                 String[] values = line.split("   ");
-                left.add(Long.parseLong(values[0]));
-                right.add(Long.parseLong(values[1]));
+                long vLeft = Long.parseLong(values[0]);
+                long vRight = Long.parseLong(values[1]);
+                left.add(vLeft);
+                right.put(vRight, right.getOrDefault(vRight, 0L) + 1L);
             }
-            left.sort(Long::compare);
-            right.sort(Long::compare);
-
-            for (int i = 0; i < left.size(); ++i) {
-                result += Math.abs(left.get(i) - right.get(i));
+            for (long vLeft : left) {
+                result += vLeft * right.getOrDefault(vLeft, 0L);
             }
 
             System.out.println("result:" + result);
